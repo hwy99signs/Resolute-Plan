b7099438126287550430aa4cba0e480069e5b64f
DigitalExpart <214820464+DigitalExpart@users.noreply.github.com>
2026-01-30 18:10:26 +0100

    fix: update versionCode to 3 for Google Play and fix button text alignment

--- app/auth.tsx
+++ app/auth.tsx
@@ -555,6 +555,7 @@ const styles = StyleSheet.create({
     color: '#3C2B63',
     fontSize: 16,
     fontWeight: '700',
+    textAlign: 'center',
   },
   modalButtonTextSecondary: {
     color: '#FFFFFF',
